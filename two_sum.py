35470be20ccfe40a9519b7367f98a7f46ee40724
natia <natia@neiron.ai>
2022-03-06 08:45:47 -0800

    changes in two_sum.py

--- two_sum.py
+++ two_sum.py
@@ -1,6 +1,4 @@
 def twoSum(self, nums: List[int], target: int) -> List[int]:
-    ind1: int = 0
-    ind2: int = 0
     val_ind_dict = {}
     for ind, elem in enumerate(nums):
         diff = target - elem
